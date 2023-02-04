8868d4d26e0ebcec978c090ea1474503468211e8
BladedShip <67158370+BladedShip@users.noreply.github.com>
2023-02-04 19:38:59 +0530

    idk

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,13 +1,36 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
-import { Colors,Parameters,GlobalStyles } from "./src/global/Styles";
+import React, { useState } from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { Colors, Parameters, GlobalStyles } from "../global/Styles";
 import { Icon } from "@rneui/base";
 import HomeHeader from "../components/HomeHeader";
 
 export default function HomeScreen() {
-    return(
+
+    const [delivery, setDelivery] = useState(true);
+
+    return (
         <View style={styles.container}>
-            <HomeHeader/>
+            <HomeHeader />
+
+            <View style={{ marginTop: 10, flexDirection: "row", justifyContent: "space-evenly" }}>
+
+                <TouchableOpacity
+                    onPress={() => setDelivery(true)}
+                >
+                    <View style={[{ backgroundColor: delivery ? Colors.green600 : Colors.prim600, }, styles.deliveryBtn]}>
+                        <Text style={[{ color: delivery ? Colors.prim600 : Colors.green600 }, styles.deliveryText]}>Delivery</Text>
+                    </View>
+                </TouchableOpacity>
+
+                <TouchableOpacity
+                    onPress={() => setDelivery(false)}
+                >
+                    <View style={[{ backgroundColor: delivery ? Colors.prim600 : Colors.green600, }, styles.deliveryBtn]}>
+                        <Text style={[{ color: delivery ? Colors.green600 : Colors.prim600 }, styles.deliveryText]}>Pickup</Text>
+                    </View>
+                </TouchableOpacity>
+
+            </View>
         </View>
     )
 }
@@ -15,6 +38,16 @@ export default function HomeScreen() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+
+    },
+    deliveryBtn: {
+        paddingHorizontal: 20,
+        borderRadius: 15,
+
+    },
+    deliveryText: {
+        margin: 5,
+        fontSize: 16,
 
     }
 })
